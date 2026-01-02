5c83fffdaa13fda16da5095ec59a3425bf106086
robk <robk@localhost>
2026-01-02 20:29:11 +0000

    [FIX] bootstrap.sh: Updated bootstrap with links to systemd details.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-REPO_URL="https://github.com/rtdevx/cicd-ansible-pull.git"
+REPO_URL="https://github.com/rtdevx/cicd-ansible-pull"
+RAW_URL="https://raw.githubusercontent.com/rtdevx/cicd-ansible-pull/main"
 PLAYBOOK="playbooks/common.yml"
 
 echo "Installing dependencies..."
 sudo apt update -y
-sudo apt install -y ansible git
+sudo apt install -y ansible git curl
 
 echo "Creating ansible-pull wrapper..."
 sudo tee /usr/local/bin/ansible-pull-wrapper >/dev/null <<EOF
@@ -18,9 +19,14 @@ EOF
 sudo chmod +x /usr/local/bin/ansible-pull-wrapper
 
 echo "Installing systemd service and timer..."
-sudo cp systemd/ansible-pull.service /etc/systemd/system/
-sudo cp systemd/ansible-pull.timer /etc/systemd/system/
 
+sudo curl -s -o /etc/systemd/system/ansible-pull.service \
+  $RAW_URL/systemd/ansible-pull.service
+
+sudo curl -s -o /etc/systemd/system/ansible-pull.timer \
+  $RAW_URL/systemd/ansible-pull.timer
+
+echo "Reloading systemd and enabling timer..."
 sudo systemctl daemon-reload
 sudo systemctl enable --now ansible-pull.timer
 
